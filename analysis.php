405b90b9ea03e5a5ee7aed398e524cfe9cffba22
Dawid Mazurek <dmazurek@olx.pl>
2017-02-21 22:39:01 +0100

    Analysis demo

--- analysis.php
+++ analysis.php
@@ -16,10 +16,6 @@ require __DIR__ . '/vendor/autoload.php';
 define('CACHE_DIR', __DIR__ . '/cache/');
 define('DATA_DIR', __DIR__ . '/data/');
 
-$serializedVectorizerPath = CACHE_DIR . 'serialized_vectorizer';
-$serializedTransformerPath = CACHE_DIR . 'serialized_transformer';
-$serializedClassifierPath = CACHE_DIR . 'serialized_classifier';
-
 ini_set('memory_limit', '1024M');
 $dataset = new CsvDataset(DATA_DIR . 'nawl-analysis-2-cols.csv', 1);
 
@@ -28,47 +24,82 @@ foreach ($dataset->getSamples() as $sample) {
     $samples[] = $sample[0];
 }
 
-if (file_exists($serializedVectorizerPath)) {
-    $vectorizer = unserialize(file_get_contents($serializedVectorizerPath));
-}
-else {
-    $vectorizer = new TokenCountVectorizer(new WordTokenizer());
-    $vectorizer->fit($samples);
-    $vectorizer->transform($samples);
-    file_put_contents($serializedVectorizerPath, serialize($vectorizer));
-}
-
-if (file_exists($serializedTransformerPath)) {
-    $tfIdfTransformer = unserialize(file_get_contents($serializedTransformerPath));
-}
-else {
-    $tfIdfTransformer = new TfIdfTransformer();
-    $tfIdfTransformer->fit($samples);
-    $tfIdfTransformer->transform($samples);
-    file_put_contents($serializedTransformerPath, serialize($tfIdfTransformer));
+function getEmotionAlias(string $emotion): string
+{
+    switch ($emotion) {
+        case 'H': return 'happiness';
+        case 'S': return 'sadness';
+        case 'A': return 'anger';
+        case 'F': return 'fear';
+        case 'D': return 'disgust';
+        case 'N': return 'neutral';
+    }
+    return 'unclassified';
 }
 
-if (file_exists($serializedClassifierPath)) {
-    $classifier = unserialize(file_get_contents($serializedClassifierPath));
-}
-else {
-    $dataset = new ArrayDataset($samples, $dataset->getTargets());
-    $randomSplit = new StratifiedRandomSplit($dataset, 0.1);
-    $classifier = new SVC(Kernel::RBF, 10000);
-    $classifier->train($randomSplit->getTrainSamples(), $randomSplit->getTrainLabels());
-    file_put_contents(
-        $serializedClassifierPath,
-        serialize($classifier)
-    );
+function getVectorizer(array $samples): \Phpml\Transformer
+{
+    $serializedVectorizerPath = CACHE_DIR . 'serialized_vectorizer';
+
+    if (file_exists($serializedVectorizerPath)) {
+        $vectorizer = unserialize(file_get_contents($serializedVectorizerPath));
+    }
+    else {
+        $vectorizer = new TokenCountVectorizer(new WordTokenizer());
+        $vectorizer->fit($samples);
+        $vectorizer->transform($samples);
+        file_put_contents($serializedVectorizerPath, serialize($vectorizer));
+    }
+
+    return $vectorizer;
 }
 
+function getTransformer(array $samples): \Phpml\Transformer
+{
+    $serializedTransformerPath = CACHE_DIR . 'serialized_transformer';
 
-$message = 'Aha w ten sposób ? 
-Myślę że nie będzie z tym problemu.
-Tylko nie wiem czy będę mógł w tej wiadomości wysłać panu linka do strony z aukcją. Bo nie wiem czy mi nie zablokuje jakiś filtr spamu czy coś.';
+    if (file_exists($serializedTransformerPath)) {
+        $tfIdfTransformer = unserialize(file_get_contents($serializedTransformerPath));
+    }
+    else {
+        $tfIdfTransformer = new TfIdfTransformer();
+        $tfIdfTransformer->fit($samples);
+        $tfIdfTransformer->transform($samples);
+        file_put_contents($serializedTransformerPath, serialize($tfIdfTransformer));
+    }
+    return $tfIdfTransformer;
+}
+
+function getClassifier(array $samples, CsvDataset $dataset): SVC
+{
+    $serializedClassifierPath = CACHE_DIR . 'serialized_classifier';
+
+    if (file_exists($serializedClassifierPath)) {
+        $classifier = unserialize(file_get_contents($serializedClassifierPath));
+    }
+    else {
+        $dataset = new ArrayDataset($samples, $dataset->getTargets());
+        $randomSplit = new StratifiedRandomSplit($dataset, 0.1);
+        $classifier = new SVC(Kernel::RBF, 10000);
+        $classifier->train($randomSplit->getTrainSamples(), $randomSplit->getTrainLabels());
+        file_put_contents(
+            $serializedClassifierPath,
+            serialize($classifier)
+        );
+    }
+    return $classifier;
+}
+
+$tfIdfTransformer = getTransformer($samples);
+$vectorizer = getVectorizer($samples);
+$classifier = getClassifier($samples, $dataset);
+
+$message = 'Witam. Dziękuję za szybką przesyłkę. Wszystko dobrze działa i synek jest zadowolony.';
 
 $newSamples= [$message];
 $vectorizer->transform($newSamples);
 $tfIdfTransformer->transform($newSamples);
-$result = $classifier->predict($newSamples);
-var_dump($result);
+$result = $classifier->predict($newSamples)[0];
+
+echo "Message: $message" . PHP_EOL .
+'Detected emotion: ' . getEmotionAlias($result) . PHP_EOL;
