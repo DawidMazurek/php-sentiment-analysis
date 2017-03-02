77a59bc7ae2e903b9faa0c810b3ae0e04e3e3f17
Dawid Mazurek <dmazurek@olx.pl>
2017-03-02 09:22:24 +0100

    Removed unnecessary else block

--- analysis.php
+++ analysis.php
@@ -13,10 +13,13 @@ use Phpml\SupportVectorMachine\Kernel;
 
 require __DIR__ . '/vendor/autoload.php';
 
+
 define('CACHE_DIR', __DIR__ . '/cache/');
 define('DATA_DIR', __DIR__ . '/data/');
 
 ini_set('memory_limit', '1024M');
+
+
 $dataset = new CsvDataset(DATA_DIR . 'nawl-analysis-2-cols.csv', 1);
 
 $samples = [];
@@ -42,14 +45,13 @@ function getVectorizer(array $samples): \Phpml\Transformer
     $serializedVectorizerPath = CACHE_DIR . 'serialized_vectorizer';
 
     if (file_exists($serializedVectorizerPath)) {
-        $vectorizer = unserialize(file_get_contents($serializedVectorizerPath));
+        return unserialize(file_get_contents($serializedVectorizerPath));
     }
-    else {
-        $vectorizer = new TokenCountVectorizer(new WordTokenizer());
-        $vectorizer->fit($samples);
-        $vectorizer->transform($samples);
-        file_put_contents($serializedVectorizerPath, serialize($vectorizer));
-    }
+
+    $vectorizer = new TokenCountVectorizer(new WordTokenizer());
+    $vectorizer->fit($samples);
+    $vectorizer->transform($samples);
+    file_put_contents($serializedVectorizerPath, serialize($vectorizer));
 
     return $vectorizer;
 }
@@ -59,14 +61,14 @@ function getTransformer(array $samples): \Phpml\Transformer
     $serializedTransformerPath = CACHE_DIR . 'serialized_transformer';
 
     if (file_exists($serializedTransformerPath)) {
-        $tfIdfTransformer = unserialize(file_get_contents($serializedTransformerPath));
+        return unserialize(file_get_contents($serializedTransformerPath));
     }
-    else {
-        $tfIdfTransformer = new TfIdfTransformer();
-        $tfIdfTransformer->fit($samples);
-        $tfIdfTransformer->transform($samples);
-        file_put_contents($serializedTransformerPath, serialize($tfIdfTransformer));
-    }
+
+    $tfIdfTransformer = new TfIdfTransformer();
+    $tfIdfTransformer->fit($samples);
+    $tfIdfTransformer->transform($samples);
+    file_put_contents($serializedTransformerPath, serialize($tfIdfTransformer));
+
     return $tfIdfTransformer;
 }
 
@@ -75,18 +77,17 @@ function getClassifier(array $samples, CsvDataset $dataset): SVC
     $serializedClassifierPath = CACHE_DIR . 'serialized_classifier';
 
     if (file_exists($serializedClassifierPath)) {
-        $classifier = unserialize(file_get_contents($serializedClassifierPath));
+        return unserialize(file_get_contents($serializedClassifierPath));
     }
-    else {
-        $dataset = new ArrayDataset($samples, $dataset->getTargets());
-        $randomSplit = new StratifiedRandomSplit($dataset, 0.1);
-        $classifier = new SVC(Kernel::RBF, 10000);
-        $classifier->train($randomSplit->getTrainSamples(), $randomSplit->getTrainLabels());
-        file_put_contents(
-            $serializedClassifierPath,
-            serialize($classifier)
-        );
-    }
+    $dataset = new ArrayDataset($samples, $dataset->getTargets());
+    $randomSplit = new StratifiedRandomSplit($dataset, 0.1);
+    $classifier = new SVC(Kernel::RBF, 10000);
+    $classifier->train($randomSplit->getTrainSamples(), $randomSplit->getTrainLabels());
+    file_put_contents(
+        $serializedClassifierPath,
+        serialize($classifier)
+    );
+
     return $classifier;
 }
 
